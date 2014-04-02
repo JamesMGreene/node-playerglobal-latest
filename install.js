a974a16969a61b8734046ad02d454b2b53bd87dd
James M. Greene <james.m.greene@gmail.com>
2014-04-02 11:15:04 -0500

    Fixed a typo in a console message

--- install.js
+++ install.js
@@ -8,7 +8,7 @@ var downloadDir = path.join(__dirname, 'lib');
 
 getRepo(repoId, downloadDir, function(err) {
   if (err) {
-    throw new Error('Failed to download "' + repoId + '"!\n\n' + err);
+    throw new Error('Failed to download "' + repoId + '"!\n' + err);
   }
-  console.log('Successfully downloaded "' + repoId + '" to: "' + downloadDir);
+  console.log('Successfully downloaded "' + repoId + '" to:\n    ' + downloadDir);
 });
