7c0eda426dc87e6f4ab1eafdb612acc376918b4a
James M. Greene <james.m.greene@gmail.com>
2014-04-03 15:31:31 -0500

    More complete 'install' workflow to maintain old versions that don't conflict

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 'use strict';
 
 var path = require('path');
+var os = require('os');
 var fs = require('fs');
 var rimraf = require('rimraf');
+var mkdirp = require('mkdirp');
 var ncp = require('ncp');
+var async = require('async');
 
 var downloadDir = path.join(__dirname, 'lib');
 
@@ -11,31 +14,76 @@ var downloadDir = path.join(__dirname, 'lib');
 module.exports = {
 
   /**
-   * The path where the "playerglobal.swc" root dir is located.
+   * 
    */
   path: downloadDir,
 
   /**
-   * Install these "playerglobal.swc" files into the appropriate location within a Flex SDK directory.
+   * Install these "playerglobal.swc" files into the appropriate location within a Flex SDK directory
    */
   install: function(FLEX_HOME, done) {
     var playersDir = path.join(FLEX_HOME, 'frameworks', 'libs', 'player');
     if (fs.existsSync(playersDir) && fs.statSync(playersDir).isDirectory()) {
-      rimraf(playersDir, function(err) {
-        if (err) {
-          return done(err);
+      var tmpDirRoot = os.tmpdir ? os.tmpdir() : os.tmpDir();
+      var tmpOldPlayersDir = path.join(tmpDirRoot, 'node-playerglobal');
+
+      var prepOldPlayerStorage = function(done) {
+        function cb(err) {
+          done(err ? new Error('Failed to prepare the storage location for the old "playerglobal.swc" library collection.\nError: ' + err) : null);
         }
+
+        if (fs.existsSync(tmpOldPlayersDir)) {
+          async.series(
+            [
+              function(done) {
+                rimraf(tmpOldPlayersDir, done);
+              },
+              function(done) {
+                mkdirp(tmpOldPlayersDir, done);
+              }
+            ],
+            cb
+          );
+        }
+        else {
+          mkdirp(tmpOldPlayersDir, cb);
+        }
+      };
+
+      var saveOldPlayers = function(done) {
+        ncp(playersDir, tmpOldPlayersDir, function(err) {
+          done(err ? new Error('Failed to maintain the old "playerglobal.swc" library collection.\nError: ' + err) : null);
+        });
+      };
+
+      var installNewPlayers = function(done) {
         ncp(downloadDir, playersDir, function(err) {
+          done(err ? new Error('Failed to install the new "playerglobal.swc" library collection.\nError: ' + err) : null);
+        });
+      };
+
+      var restoreNonConflictingOldPlayers = function(done) {
+        ncp(tmpOldPlayersDir, playersDir, { clobber: false }, function(err) {
           if (err) {
-            return done(err);
+            console.warn('WARNING: Failed to restore any non-conflicting files from the old "playerglobal.swc" library collection.\nError: ' + err);
           }
           done();
         });
-      });
+      };
+
+      async.series(
+        [
+          prepOldPlayerStorage,
+          saveOldPlayers,
+          installNewPlayers,
+          restoreNonConflictingOldPlayers
+        ],
+        done
+      );
     }
     else {
       done(new TypeError('The FLEX_HOME provided does not contain the expected directory structure'));
     }
   }
-
 };
+
